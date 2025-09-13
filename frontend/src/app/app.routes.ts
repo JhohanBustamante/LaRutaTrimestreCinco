f2788d392d2fa304a1106959f22862ae13272bcc
KevinLop2524 <kevinlop2524@gmial.com>
2025-09-13 09:54:47 -0500

    se agrega el componente favoritos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { BlogPrincipal } from './componentes/blog-principal/blog-principal';
 import { Perfil } from './componentes/perfil/perfil';
 import { ServicioComponent } from './componentes/servicio/servicio.component';
 import { ComunidadComponent } from './componentes/comunidad/comunidad.component';
+import { FavoritosComponent } from './componentes/favoritos/favoritos.component';
 
 export const routes: Routes = [
     { path: "", component: InicioSesion, pathMatch: "full" },
     { path: "blog", component: BlogPrincipal },
     { path: "perfil", component: Perfil },
     { path: "servicios", component: ServicioComponent },
-    { path: "comunidades", component: ComunidadComponent }
+    { path: "comunidades", component: ComunidadComponent },
+    { path: "favoritos", component: FavoritosComponent }
 ];
